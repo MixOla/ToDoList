ed0e805779f605caaf045218defb2a9d99bc9177
@omi-coder <omi-ufa@yandex.ru>
2023-01-14 18:55:21 +0300

    added core

--- core/models.py
+++ core/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+    # class Meta:
+    #     verbose_name = 'Пользователь'
+    #     verbose_name_plural = 'Пользователи'
+
+
+
